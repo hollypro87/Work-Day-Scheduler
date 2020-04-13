a7e826e63b002376ec4211cd444736ee5546b18c
Holly Prothe <hollyprothe1@gmail.com>
2020-04-12 20:16:30 -0500

    working code. just need to add readme

--- script.js
+++ script.js
@@ -1,104 +1,58 @@
-const m = moment();
-var words;
-var hourInfo;
+function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`#text${key}`).text(value);
+    }
+}
 
-console.log(m.format("dddd, MMMM Do"));
-
-$("#currentDay").ready(function(){
-	var date=new Date();
-	$("#btn1").click(function(){
-		var val=date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
-		$("#printDate").text(val);
-	});
-	$("#btn2").click(function(){
-		var val=(date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear();
-		$("#printDate").text(val);
-	});
-	$("#btn3").click(function(){
-		var months=["JAN","FEB","MAR","APR","MAY","JUN","JUL",
-		"AUG","SEP","OCT","NOV","DEC"];
-		var val=date.getDate()+" "+months[date.getMonth()]+" "+date.getFullYear();
-		$("#printDate").text(val);
-	});				
-});
-
-
-$(document).ready( function() {
-    colorChange ();
-    renderText ();
-});
-
-function colorChange () {
+$( document ).ready(function() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    for (let i = 9; i < 18; i++) {
     
-    var realTime = moment().hours();
-    console.log("Current Time" + realTime);
-    
-    $(".input").each(function () {
-        var timeTest = parseInt($(this).attr("id"));
-        console.log(timeTest);
+        var row = $(`<div data-time=${i} id='${i}' class="row">`);
+
+        var col1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
+
+        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);        
+       
+        var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
         
-        if (realTime > timeTest) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        } else if (realTime < timeTest) {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        } else {
-            $(this).removeClass("future");
-            $(this).removeClass("past");
-            $(this).addClass("present");
+        row.append(col1);
+        row.append(col2);
+        row.append(col3);
+
+        $(".container").append(row);
+
+        getLocalStorage(i);
+    }
+
+    function formatAMPM(hours) {
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + ampm;
+    }
+formatAMPM();
+
+function updateColors(){
+        var currentTime = new Date().getHours();
+        for (var i = 9; i < 18; i++) { 
+        console.log(currentTime, $(`#${i}`).data("time"));
+         if ($(`#${i}`).data("time") == currentTime){
+            $(`#text${i}`).addClass( "present");
+        } else if (currentTime < $(`#${i}`).data("time")) {
+            $(`#text${i}`).addClass( "future");
         }
-    });
+    }
 }
 
-$(".saveBtn").click(function() {
-    words = $(this).siblings(".input").val();
-    console.log(words);
-    hourInfo = $(this).siblings(".hour").text();
-    console.log(hourInfo);
-    localStorage.setItem(hourInfo, JSON.stringify(words));
-    
-    colorChange ();
-    renderText ();
-})
+setInterval(function() {
+    updateColors();
+}, 1000);
 
-
-function renderText () {
-    var saveWords9 = JSON.parse(localStorage.getItem("9:00 am"));
-    $("#9").val("");
-    $("#9").val(saveWords9);
-    
-    var saveWords10 = JSON.parse(localStorage.getItem("10:00 am"));
-    $("#10").val("");
-    $("#10").val(saveWords10);
-    
-    var saveWords11 = JSON.parse(localStorage.getItem("11:00 am"));
-    $("#11").val("");
-    $("#11").val(saveWords11);
-    
-    var saveWords12 = JSON.parse(localStorage.getItem("12:00 pm"));
-    $("#12").val("");
-    $("#12").val(saveWords12);
-    
-    var saveWords1 = JSON.parse(localStorage.getItem("1:00 pm"));
-    $("#13").val("");
-    $("#13").val(saveWords1);
-
-    var saveWords2 = JSON.parse(localStorage.getItem("2:00 pm"));
-    $("#14").val("");
-    $("#14").val(saveWords2);
-
-    var saveWords3 = JSON.parse(localStorage.getItem("3:00 pm"));
-    $("#15").val("");
-    $("#15").val(saveWords3);
-
-    var saveWords4 = JSON.parse(localStorage.getItem("4:00 pm"));
-    $("#16").val("");
-    $("#16").val(saveWords4);
-
-    var saveWords5 = JSON.parse(localStorage.getItem("5:00 pm"));
-    $("#17").val("");
-    $("#17").val(saveWords5);
-}
+var saveBtn = $('.saveBtn');
+saveBtn.on('click', function(){
+    let eventId = $(this).attr('id');
+    let eventText = $(this).parent().siblings().children('.description').val();
+    localStorage.setItem(eventId, eventText);
+});});
